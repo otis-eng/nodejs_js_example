4fe9d77671792dfb94de10f94b5bdc2a33eedf84
Phu <71256773+otis-eng@users.noreply.github.com>
2022-06-23 22:36:44 +0700

    add redis

--- api/controller.js
+++ api/controller.js
@@ -1,5 +1,13 @@
-const userController = (req, res) => {
-  return res.status(200).send({ message: "Success", data: req.body });
+// const { cache } = require("joi");
+
+// const { cache } = require("joi");
+
+// const cache = require("redis");
+const userController = async (req, res) => {
+  console.log(req.body);
+  //cache.set("key", "value");
+  const reulst = await cache.get("key");
+  return res.json(reulst);
 };
 
 module.exports = userController;
