f42e81984e0a60d798c9ee68ce17a4975bcc5d31
Phu <71256773+otis-eng@users.noreply.github.com>
2022-06-23 22:37:09 +0700

    add redis

--- api/router.js
+++ api/router.js
@@ -4,6 +4,6 @@ const routers = express.Router();
 const UserValidate = require("../joi/user.joi");
 const userController = require("./controller");
 
-routers.post("/user", UserValidate, userController);
+routers.post("/user", userController);
 
 module.exports = routers;
