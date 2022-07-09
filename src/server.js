94b1c7e7343dbb8576c0f330f805b819c4722c9f
Phu <71256773+otis-eng@users.noreply.github.com>
2022-07-09 21:53:07 +0700

    update revert

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const dotenv  = require("dotenv");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors  = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 dotenv.config();
 
-
-const postRouter = require("./router/post.router")
+const postRouter = require("./router/post.router");
 
 const app = express();
 
@@ -14,22 +13,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 // config datbase
-const dbURL = process.env.MONGODB_URL;
-mongoose.connect(
-    dbURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-).then(
-    () => console.log("connect to database success!")
-).catch((err) => console.log(err));
+// const dbURL = process.env.MONGODB_URL;
+// mongoose.connect(
+//     dbURL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// ).then(
+//     () => console.log("connect to database success!")
+// ).catch((err) => console.log(err));
 
+app.use("/api/post", postRouter);
 
-app.use("/api/post",postRouter)
-
-
-app.listen(process.env.PORT,() =>{
-    console.log("App listen port: "+ process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("App listen port: " + process.env.PORT);
 });
